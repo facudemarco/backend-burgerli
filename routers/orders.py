21b2604ff410c4d93ae35644e14674c1defd1734
Facundo <fac.demarco37@gmail.com>
2026-05-28 18:40:31 -0300

    Fix: build order products array

--- routers/orders.py
+++ routers/orders.py
@@ -149,12 +149,7 @@ async def change_order_local(order_id: str, body: ChangeLocalBody):
                 {"order_id": order_id}
             )
 
-            product_parts = prod_result.scalars().all()
-
-            if product_parts:
-                product_list = ["{" + ", ".join(product_parts) + "}"]
-            else:
-                product_list = []
+            product_list = [prod_row['products'] for prod_row in prod_result.mappings().all()]
                 
             # Actualizar local
             conn.execute(
